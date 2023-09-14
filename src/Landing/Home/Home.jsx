198940b2e1195d8e622eb27361234222d6e88637
T_NahuelReboledo <thiagoreboledo8@gmail.com>
2023-09-14 09:11:19 -0300

    acomodo de fuentes

--- src/Landing/Home/Home.jsx
+++ src/Landing/Home/Home.jsx
@@ -11,9 +11,13 @@ function Home() {
             <img className={style.bg_blur1} src={Rectangle4} alt="" />
 
             <div className={style.home_page_container}>
+
                <div className={style.logo_container}>
+
                   <img className={style.logo_TNR} src={TNR_logo} alt="logo TNR" />
+
                   <h1 className={style.name_tnr}>Thiago Nahuel Reboledo</h1>
+                  
                </div>
 
                <img className={style.portfolio_text} src={Portfolio_text} />
